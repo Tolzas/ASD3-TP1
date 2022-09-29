487716ada85fdfdcc6326fb4a9e40a21d2931d64
victo <victor.zesko12@gmmail.com>
2022-09-29 23:52:47 +0200

    V1

--- src/com/mycompany/TP1/Main.java
+++ src/com/mycompany/TP1/Main.java
@@ -1,7 +1,24 @@
 package com.mycompany.TP1;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String... args){
-        System.out.println("Start");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Combien voulez-vous de case dans votre tableau : ");
+        int caseTab = scan.nextInt();
+        System.out.println("Vous avez choisie de crer un tableau à " + caseTab + " case.");
+
+        int [] S  = new int [caseTab];
+
+        for(int i = 0 ;i<caseTab;i++){
+            System.out.println("Veuillez rentrer la valeur que vous vouler rentrer à la place " + (i+1) + " dans votre tab de int :");
+            int value = scan.nextInt();
+            S[i] = value;
+            System.out.println(S[i]);
+        }
     }
 }
